94a099bcbc8ce17bb27cad1247266d7a90cb7c6f
利开团 <likaituan@wecash.net>
2016-06-20 17:03:10 +0800

    quick commit

--- bin/genui.js
+++ bin/genui.js
@@ -6,22 +6,24 @@
 (function(exp) {
     "use strict";
     var fs = require("fs");
-    var tp = require("../../nova/h5template");
+    var tp = require("./h5template");
 
     //添加View页面
     exp.addFile = function(name){
-        var tplCode = fs.readFileSync(name+"/ui.html").toString();
+        var tplCode = fs.readFileSync(`ui/${name}/ui.html`).toString();
         var jsCode = tp.getJsCode(tplCode);
         var fun = 'exp.getTemplate = function($){'+jsCode+'};';
-        var code = fs.readFileSync(name+"/ui.js").toString();
+        var code = fs.readFileSync(`ui/${name}/ui.js`).toString();
         code = code.replace("\"use strict\";", "\"use strict\";\n\n" + fun + "\n\n");
-        var id = "sys.ui." + name;
-        code = exp.addCode(id, code);
+        var mid = "sys.ui." + name;
+        code = exp.addCode(mid, code);
 
+        /*
         code = code.replace('req("sys.ui.'+name+'.pic")', function(){
             var picCode = fs.readFileSync("public/seekjs/ui/"+name+"/pic.js").toString();
             return picCode.replace(/^[\s\S]*?define\(\"sys\.ui\.\w+\.pic\"\s*,\s*\{/, "{").replace(/\}\)\;$/,"}");
         });
+        */
         exp.saveFile(name, code);
     };
 
@@ -32,7 +34,7 @@
 
     //保存文件
     exp.saveFile = function(folder, code){
-        fs.writeFileSync(folder+"/ui.min.js", code);
+        fs.writeFileSync(`ui/${folder}/ui.min.js`, code);
     };
 
     exp.init = function(){
@@ -41,13 +43,13 @@
 		var forder = process.argv[2];
 		if(forder){
 			exp.addFile(forder);
-		}else{
-			throw "请输入名称";
-		}
-        var endTime = Date.now();
 
-        var time = endTime - startTime;
-        console.log("merge complete, use time "+time+"ms");
+            var endTime = Date.now();
+            var time = endTime - startTime;
+            console.log("merge complete, use time "+time+"ms");
+		}else{
+			console.log("input a name please!");
+		}
     };
 
     exp.init();
